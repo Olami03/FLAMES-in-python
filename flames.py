5039ad12fa11052f3809969ff51d424fdc4c3967
Atul Kushwaha <developeratulkushwaha@gmail.com>
2021-11-09 11:26:08 +0530

    logic improved

--- flames.py
+++ flames.py
@@ -1,24 +1,29 @@
+#printing banner
 print('''
-███████╗██╗░░░░░░█████╗░███╗░░░███╗███████╗░██████╗    ,d88b.d88b,
-██╔════╝██║░░░░░██╔══██╗████╗░████║██╔════╝██╔════╝    88888888888
-█████╗░░██║░░░░░███████║██╔████╔██║█████╗░░╚█████╗░    `Y8888888Y'
-██╔══╝░░██║░░░░░██╔══██║██║╚██╔╝██║██╔══╝░░░╚═══██╗      `Y888Y'
-██║░░░░░███████╗██║░░██║██║░╚═╝░██║███████╗██████╔╝        `Y'
-╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░    
+███████╗██╗                 █████╗   ███╗         ███╗███████╗███████╗
+██╔════╝██║              ██╔══██╗████╗   ████║██╔════╝██╔════╝
+█████╗      ██║              ███████║██╔████╔██║█████╗     ███████╗
+██╔══╝      ██║              ██╔══██║██║╚██╔╝██║██╔══╝     ╚════██║
+██║              ███████╗██║      ██║██║ ╚═╝     ██║███████╗███████║
+╚═╝             ╚══════╝╚═╝      ╚═╝╚═╝               ╚═╝╚══════╝╚══════╝
 ''')
-# Author = coderatul
-# Taking names as input from user
-user = input("Enter your name :").casefold().strip()
-anotherperson = input("Enter another person's name :").casefold().strip()
-assert user.isalpha(),"Excuse me : ------name should only contain alphabets------"
-assert anotherperson.isalpha(),"Excuse me :  ------name should only contain alphabets------"
-if user == anotherperson:
-  print("You are already in love with your self")
+user = input("Enter your name :").casefold().replace(" ", "")
+anotherperson = input("Enter another person's name :").casefold().replace(" ", "")
+assert user.isalpha(),"------name should only contain alphabets------"
+assert anotherperson.isalpha(),"------name should only contain alphabets------"
+#logic to get len of common chr as per flames need
+common_chr_len = (len(user)+len(anotherperson) - len(set(user)&set(anotherperson)) * 2 )
+#finding relation
+if common_chr_len>0:
+  relations = ["Friends","Lovers","in Affection","going to Marry eachother","Enemies","siblings"]
+  while len(relations)>1:
+    index = common_chr_len%len(relations) -1
+    if index >=0:
+      l = relations[:index]
+      r = relations[index+1:]
+      relations = r+l
+    else:
+      relations = relations[:len(relations)-1]
+  print(user,"and",anotherperson,"are",relations[0])
 else:
-  a = (len(user)+len(anotherperson) - len(set(user)&set(anotherperson)) * 2 )
-  flames = {1:"Siblings",2:"Enemies",3:"Enemies",4:"Enemies",5:"Friends",
-            6:"Going to marry each other",7:"Enemies",8:"In affection",9:"Enemies",
-            10:"In Love",11:"Going to marry each other",12:"In affection",
-            13:"Siblings",14:"Friends",15:"Going to marry each other",16:"Friends",
-            17:"Siblings",18:"Friends",19:"Siblings",20:"Enemies"}
-  print("According to FLAMES you and",anotherperson,"are:",flames[a])
+  print("may be you are a tester or your's and his/her name is same")
